5fe4283fff65c6c32795bd04dc28b934999f9ac9
Unicorn-7-Ninja <abouseir.meriem@gmail.com>
2023-03-29 18:20:32 -0700

    t

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,31 @@
 #include "main.h"
-/**
- * cap_string - do thing
- * @str: string
- * Return: string
- */
-char *cap_string(char *str)
-{
-	int i = 0;
-	int cap_next = 1; /* capitalize next character */
 
-		while (str[i] != '\0')
+/**
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + i))
 	{
-		if (cap_next && isalpha(str[i]))
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			str[i] = toupper(str[i]);
-			cap_next = 0;
-		}
-		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-		 str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-		 str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-		 str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-		 str[i] == '}')
-		{
-			cap_next = 1;
-		}
-		else
-		{
-			cap_next = 0;
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
 		}
 		i++;
 	}
-	return (str);
+	return (s);
 }
