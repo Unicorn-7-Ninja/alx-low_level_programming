08c9212e157ae9679f2025ecd22020c23e111a0d
Unicorn-7-Ninja <abouseir.meriem@gmail.com>
2023-03-29 18:28:32 -0700

    t

--- 0x04-more_functions_nested_loops/101-prime_number.c
+++ 0x04-more_functions_nested_loops/101-prime_number.c
@@ -1,21 +1,28 @@
 #include "main.h"
+
 /**
-* print_number - prints an integer.
-* @n: integer to print.
-* Return: void
+ * print_number - function that prints an integer
+ * @n : input int parameter
+ * Return : Always 0
 */
 void print_number(int n)
 {
-	unsigned int m = n;
+	unsigned int x;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		m = -m;
+		x = -n;
+		_putchar ('-');
 	}
-	if (m >= 10)
+	else
 	{
-		print_number(m / 10);
+		x = n;
+
 	}
-	_putchar(m % 10 + '0');
+
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
 }
