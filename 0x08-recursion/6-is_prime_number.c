c58c647f12b630b6c1aca7a5e24b70990d25527e
Unicorn-7-Ninja <abouseir.meriem@gmail.com>
2023-04-05 18:30:24 -0700

    lm

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * is_prime_number - checks if a number is a prime number
  * @n: the number to check
@@ -10,7 +9,6 @@ int is_prime_number(int n)
 {
     return (prime(n, 2));
 }
-
 /**
  * prime - helping function
  * @n: the number to check
