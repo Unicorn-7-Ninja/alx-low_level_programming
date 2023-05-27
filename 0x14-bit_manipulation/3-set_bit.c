3364d6ffcbe2f710d28f42b4ab142fc8f1668366
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2023-05-27 19:36:21 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * set_bit node in - sets a bit at a given index to 1
- * @n: pointer to the number to change
- * @index: index of the bit to set to 1
+ * set_bit - met un bit à 1 à un index donné
+ * @n: pointeur vers le nombre à modifier
+ * @index: indice du bit à mettre à 1
  *
- * Return: 1 for success, -1 for failure
+ * Return: 1 en cas de succès, -1 en cas d'échec
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
 
-	*n = ((1UL << index) | *n);
+	*n |= (1UL << index);
 	return (1);
 }
