39ecf1e42554d5befd5406890a5ae84d531e46b0
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2023-05-27 19:33:54 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * clear_bit node in - sets the value of a given bit to 0
- * @n: pointer to the number to change
- * @index: index of the bit to clear
+ * clear_bit - met la valeur d'un bit donné à 0
+ * @n: pointeur vers le nombre à modifier
+ * @index: indice du bit à effacer
  *
- * Return: 1 for success, -1 for failure
+ * Return: 1 en cas de succès, -1 en cas d'échec
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
 
-	*n = (~(1UL << index) & *n);
+	*n &= ~(1UL << index);
 	return (1);
 }
