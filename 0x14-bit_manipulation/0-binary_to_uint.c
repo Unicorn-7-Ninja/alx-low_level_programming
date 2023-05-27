4de08f57c7bbeb3f4925bdfa36e2c3d54dd323af
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2023-05-27 18:28:01 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,28 @@
 #include "main.h"
 
+#define MAX_BINARY_LENGTH 32
+#define BINARY_ZERO '0'
+#define BINARY_ONE '1'
+
 /**
- * binary_to_uint node in - converts a binary number to unsigned int
+ * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
- * Return: the converted number
+ * Return: the converted number, or 0 if b is NULL or contains invalid characters
  */
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int dec_val = 0;
 
-	if (!b)
+	if (!b || strlen(b) > MAX_BINARY_LENGTH)
 		return (0);
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] != BINARY_ZERO && b[i] != BINARY_ONE)
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - BINARY_ZERO);
 	}
 
 	return (dec_val);
