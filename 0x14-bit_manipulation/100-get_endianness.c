70c89079570dce05f600c256335116a54f4d65bd
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2023-07-07 00:23:22 +0100

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 
+ * Check : system is big or little endian
+ * 
+ * Always return: 0 if big endian, 1 if little endian
+ *
+ **/
+
+int get_endianness(void)
+
+{
+	int num;
+	char *adrs;
+
+	num = 2;
+	adrs = (char *)&num;
+	if (*adrs)
+		return (1);
+	return (0);
+}
