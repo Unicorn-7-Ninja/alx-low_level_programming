2b3450f91ba4c2f153b29585345e248ddb48bab8
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2024-02-03 00:39:25 +0100

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,13 @@
 #include "hash_tables.h"
 
+
 /**
- * hash_djb2 - the djb2 algorithm
- * @str: Char pointer of data to be hashed
- * Source: http://www.cse.yorku.ca/~oz/hash.html
- * Return: Unsigned long int
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
  */
+
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
